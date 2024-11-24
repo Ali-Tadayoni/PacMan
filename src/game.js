0320dabd41e172960830e5bf17fbcb5433e76ae6
“Ali <tadayoni.ali.enayat@gmail.com>
2024-11-24 23:55:21 +0330

    Draw Ghosts

--- src/game.js
+++ src/game.js
@@ -19,11 +19,20 @@ let wallOfset = (oneBlockSize - wallSpaceWidth) / 2; // 0.9
 let wallInnerColor = "black";
 let foodColor = "#FEB897";
 let score = 0;
+let ghosts = [];
+let ghostCount = 4;
 
 const DIRECTION_RIGHT = 4;
 const DIRECTION_UP = 3;
 const DIRECTION_LEFT = 2;
 const DIRECTION_BOTTOM = 1;
+
+let ghostImageLocations = [
+  { x: 0, y: 0 },
+  { x: 176, y: 0 },
+  { x: 0, y: 121 },
+  { x: 176, y: 121 },
+];
 
 let map = [
   [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
@@ -81,8 +90,14 @@ let drawScore = () => {
   canvasContext.fillText(
     "Score: " + score,
     0,
-    oneBlockSize * (map.length + 1) + 3
+    oneBlockSize * (map.length + 1) + 10
   );
+};
+
+let drawGhosts = () => {
+  for (let i = 0; i < ghosts.length; i++) {
+    ghosts[i].draw();
+  }
 };
 
 let draw = () => {
@@ -91,6 +106,7 @@ let draw = () => {
   drawScore();
   drawFoods();
   pacman.draw();
+  drawGhosts();
 };
 let gameInterval = setInterval(gameLoop, 1000 / fps);
 
@@ -159,7 +175,27 @@ let createNewPacman = () => {
   );
 };
 
+let createGhosts = () => {
+  ghosts = [];
+  for (let i = 0; i < ghostCount * 2; i++) {
+    let newGhost = new Ghost(
+      9 * oneBlockSize + (i % 2 == 0 ? 0 : 1) * oneBlockSize,
+      10 * oneBlockSize + (i % 2 == 0 ? 0 : 1) * oneBlockSize,
+      oneBlockSize,
+      oneBlockSize,
+      pacman.speed / 2,
+      ghostImageLocations[i % 4].x,
+      ghostImageLocations[i % 4].y,
+      124,
+      116,
+      6 + i
+    );
+    ghosts.push(newGhost);
+  }
+};
+
 createNewPacman();
+createGhosts();
 gameLoop();
 
 window.addEventListener("keydown", (event) => {
